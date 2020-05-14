436e22ec68978df25c037316e5fe46786303984f
Roman Nikitin <nikrom2012@me.com>
2020-05-14 14:55:32 +0300

    Added animations

--- visualisation/vis_gen.py
+++ visualisation/vis_gen.py
@@ -1,60 +1,79 @@
 #!/usr/bin/env python3
 
-import secrets
 import subprocess
 import sys
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 
 import numpy as np
-from PIL import Image
+from matplotlib import pyplot as plt
+from matplotlib import animation
 from lxml import etree
 
 BASE_DIR = Path(__file__).absolute().resolve().parents[1]
 EXEC_PATH = BASE_DIR / 'Bin' / 'Debug' / 'PathPlanning'
-TESTS_DIR = BASE_DIR / 'TestData'
+
+PER_PIXEL = 9
 
 
-def parse(test, stderr):
+def set_pixel(blocked, data, i, j, pix, sz=PER_PIXEL):
+    if (i, j) in blocked:
+        return
+    coord = data[PER_PIXEL * i:PER_PIXEL * i + sz, PER_PIXEL * j:PER_PIXEL * j + sz]
+    for di in range(sz):
+        for dj in range(sz):
+            coord[di, dj] = pix
+
+
+def parse(test, stderr, dst_path):
     rows = list(map(lambda x: list(map(int, x.text.split(' '))),
                     test.find('map').find('grid').findall('row')))
-    data = np.zeros((512, 512, 3), dtype=np.uint8)
+    data = np.zeros((PER_PIXEL * len(rows), PER_PIXEL * len(rows[0]), 3), dtype=np.uint8)
 
+    fig = plt.figure()
+    images = []
+
+    blocked = set()
     for i, row in enumerate(rows):
         for j, col in enumerate(row):
             if col:
-                data[i, j] = [0, 0, 0]
+                set_pixel(blocked, data, i, j, [0, 0, 0])
+                blocked.add((i, j))
             else:
-                data[i, j] = [255, 255, 255]
+                set_pixel(blocked, data, i, j, [255, 255, 255])
 
-    log = sorted(stderr.split('\n')[:-1])
-    for line in log:
+    log = stderr.split('\n')[:-1]
+
+    approx_length = 20 * 1000
+    interval = 20
+    total_length = len(log) * interval
+    draw_interval = max(1, total_length // approx_length)
+
+    for step, line in enumerate(log):
         i, j = list(map(int, line.split(' ')[-2:]))
-        big = False
+        block = False
         if 'LOOK' in line:
             color = [0, 255, 0]
         elif 'OPEN' in line:
             color = [255, 0, 0]
         elif 'START' in line:
-            big = True
+            block = True
             color = [0, 0, 255]
         elif 'TERM' in line:
-            big = True
+            block = True
             color = [255, 0, 255]
         else:
             assert False
 
-        if not big:
-            data[i, j] = color
-            continue
+        set_pixel(blocked, data, i, j, color)
+        if block:
+            blocked.add((i, j))
 
-        for di in range(-5, 6):
-            for dj in range(-5, 6):
-                data[i + di, j + dj] = color
+        if step % draw_interval == 0 or step == len(log) - 1:
+            images.append([plt.imshow(data, animated=True)])
 
-    image = Image.fromarray(data)
-    image.save(f'/tmp/vis_{secrets.token_hex(10)}.png')
-    image.show()
+    ani = animation.ArtistAnimation(fig, images, interval=interval, blit=True, repeat=False)
+    ani.save(dst_path)
 
 
 if __name__ == '__main__':
@@ -62,23 +81,59 @@ if __name__ == '__main__':
         print(f'Usage: {sys.argv[0]} test_file')
         exit(1)
 
-    test_path = (TESTS_DIR / sys.argv[1]).resolve()
+    test_path = (Path().cwd() / sys.argv[1]).resolve()
     test_data = test_path.read_text()
     root = etree.fromstring(test_data)
-
     root.find('options').find('loglevel').text = '1.5'
-    tmp_f = NamedTemporaryFile(suffix='.xml', delete=False)
 
-    etree.ElementTree(root).write(tmp_f)
+    for algorithm in ['astar', 'dijkstra', 'jp_search']:
+        if algorithm == 'dijkstra':
+            # dijkstra doesn't use metrics or breaking ties
+            metrics = ['diagonal']
+            breaking_ties = ['g-min']
+        else:
+            metrics = ['diagonal', 'manhattan', 'euclidean', 'chebyshev']
+            breaking_ties = ['g-min', 'g-max']
 
-    tmp_f.close()
+        for metric in metrics:
+            for bt in breaking_ties:
+                for allow_diagonal in [0, 1]:
+                    for cut_corners in [0, 1]:
+                        if cut_corners and not allow_diagonal:
+                            continue
+                        for allow_squeeze in [0, 1]:
+                            if allow_squeeze and (not cut_corners or not allow_diagonal):
+                                continue
 
-    p = subprocess.Popen(
-        [EXEC_PATH, tmp_f.name],
-        cwd=BASE_DIR,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    out, err = p.communicate()
+                            with NamedTemporaryFile(suffix='.xml', delete=False) as tmp_f:
+                                algo_el = root.find('algorithm')
+                                algo_el.find('searchtype').text = algorithm
+                                algo_el.find('metrictype').text = metric
+                                algo_el.find('breakingties').text = bt
+                                algo_el.find('allowdiagonal').text = str(allow_diagonal)
+                                algo_el.find('cutcorners').text = str(cut_corners)
+                                algo_el.find('allowsqueeze').text = str(allow_squeeze)
+                                etree.ElementTree(root).write(tmp_f)
+                                tmp_f.close()
 
-    parse(root, err.decode())
+                                p = subprocess.Popen(
+                                    [EXEC_PATH, tmp_f.name],
+                                    cwd=BASE_DIR,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE,
+                                )
+                                out, err = p.communicate()
+
+                            fname = f'ani_{algorithm}_{metric}_{bt}'
+                            if allow_diagonal:
+                                fname += '_allowdiagonal'
+                            if cut_corners:
+                                fname += '_cutcorners'
+                            if allow_squeeze:
+                                fname += '_allowsqueeze'
+
+                            dst_path = (Path(__file__).resolve().parent / 'animations' / fname)
+                            dst_path = dst_path.with_suffix('.mp4')
+
+                            parse(root, err.decode(), dst_path)
+                            print('Done', dst_path)
